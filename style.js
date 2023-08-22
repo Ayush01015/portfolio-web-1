70a7957891917832c8c8a2b5a13462e8c8467420
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-08-22 15:00:56 +0530

    Circle following Cursor Added

--- style.js
+++ style.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove",(details)=>{
+        // console.log("x=",details.clientX,"y=",details.clientY);
+        document.querySelector("#mini-circle").style.transform = `translate(${details.clientX}px,${details.clientY}px)`
+    })
+}
+
+circleMouseFollower();

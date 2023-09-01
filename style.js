1880de4f86867c1f9c0c9249898dec7f495466c0
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-09-01 13:55:22 +0530

    last image animation added

--- style.js
+++ style.js
@@ -31,26 +31,31 @@ function firstPageAnim(){
 
 
 
-
+var timeout;
 function skewCircleAnimation(){
-    let xscale = 1;
-    let yscale = 1;
+    let defaultXscale = 1;
+    let defaultYscale = 1;
 
     let xPrev = 0;
     let yPrev = 0;
 
     window.addEventListener("mousemove",(dets)=>{
+        clearTimeout(timeout);
         /**
          * gsap.utils.clamp(range1,range2,value)
          * clamp method to limit the values of xscale and yscale between 0.8 and 1.2.
          */
-        xscale = gsap.utils.clamp(0.8,1.2,dets.clientX - xPrev);
-        yscale = gsap.utils.clamp(0.8,1.2,dets.clientY - yPrev);
+        let xscale = gsap.utils.clamp(0.8,1.2,dets.clientX - xPrev);
+        let yscale = gsap.utils.clamp(0.8,1.2,dets.clientY - yPrev);
         
         xPrev = dets.clientX;
         yPrev = dets.clientY;
 
         circleMouseFollower(xscale,yscale);
+
+        timeout = setTimeout(()=>{
+            document.querySelector("#mini-circle").style.transform = `translate(${dets.clientX}px,${dets.clientY}px) scale(${defaultXscale},${defaultYscale})`
+        },100)
         
     })
 }
@@ -60,7 +65,32 @@ function circleMouseFollower(xscale,yscale){
         document.querySelector("#mini-circle").style.transform = `translate(${details.clientX}px,${details.clientY}px) scale(${xscale},${yscale})`
     })
 }
-circleMouseFollower();
+
+
+let diffRot = 0;
+let rotate = 0;
+
+document.querySelectorAll(".element")
+.forEach((elem)=>{
+    elem.addEventListener("mouseleave",(dets)=>{
+        gsap.to(elem.querySelector("img"),{
+            opacity:0,
+            ease:Power3,
+        })
+    })
+    elem.addEventListener("mousemove",(dets)=>{
+        var diff =(dets.clientY - elem.getBoundingClientRect().top);
+        diffRot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        gsap.to(elem.querySelector("img"),{
+            opacity:1,
+            ease:Power3,
+            top:diff,
+            left:dets.clientX,
+            rotate:gsap.utils.clamp(-20,20,diffRot*0.5),
+        })
+    })
+})
+
 firstPageAnim();
 skewCircleAnimation();
-
